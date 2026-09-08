9a291b3d32901ad86243e106b03cd5848adb5e59
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-09-08 17:44:25 +0600

    Added: Services-Section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,10 +3,25 @@ import Service from '../assets/service.png'
 
 const Services = () => {
     return (
-        <div>
-            <img src={Service} alt="" />
-            <h2 className='text-4xl font-bold text-center mb-2 text-green-700'>Services</h2>
-            <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique doloribus voluptates ullam officiis,  repellendus?</p>
+        <div className='my-[40px] container mx-auto '>
+            <h2 className='text-green-600 mb-4 font-bold text-3xl'>Services</h2>
+            <div className='grid grid-cols-3 gap-4 py-8 '>
+                <div className='shadow-2xl rounded-md py-4'>
+                    <img src={Service} alt="" className='mx-auto' />
+                    <h2 className='text-4xl font-bold text-center mb-2 text-green-700'>Services</h2>
+                    <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique doloribus voluptates ullam officiis,  repellendus?</p>
+                </div>
+                <div className='shadow-2xl rounded-md py-4'>
+                    <img src={Service} alt="" className='mx-auto' />
+                    <h2 className='text-4xl font-bold text-center mb-2 text-green-700'>Services</h2>
+                    <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique doloribus voluptates ullam officiis,  repellendus?</p>
+                </div>
+                <div className='shadow-2xl rounded-md py-4'>
+                    <img src={Service} alt="" className='mx-auto' />
+                    <h2 className='text-4xl font-bold text-center mb-2 text-green-700'>Services</h2>
+                    <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique doloribus voluptates ullam officiis,  repellendus?</p>
+                </div>
+            </div>
         </div>
     );
 };
